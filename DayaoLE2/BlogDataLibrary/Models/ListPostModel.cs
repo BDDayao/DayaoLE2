c714ccd815da111130b3a28132b043958b80b5d3
BDDayao <155168852+BDDayao@users.noreply.github.com>
2025-11-03 22:49:00 +0800

    Add files via upload

---
+++ DayaoLE2/BlogDataLibrary/Models/ListPostModel.cs
@@ -0,0 +1,10 @@
+﻿public class ListPoseModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public DateTime DateCreated { get; set; }
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}

dd180d68b59624e4f2768ea717ab7e91e356ee55
BDDayao <155168852+BDDayao@users.noreply.github.com>
2025-11-03 22:53:14 +0800

    Add files via upload

---
+++ DayaoBlogDB/dbo/Tables/Posts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table2]
+(
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [UserId] INT NOT NULL,
+    [Title] NVARCHAR(150) NOT NULL,
+    [Body] TEXT NOT NULL,
+    [DateCreated] DATETIME2(7) NOT NULL,
+    CONSTRAINT [FK_Posts_Users] FOREIGN KEY ([UserId]) REFERENCES [Users]([Id]),
+)
